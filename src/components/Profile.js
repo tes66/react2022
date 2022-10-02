b77602e0cfbba67a62b0dddd3a8b91e54f04965a
tes66 <tes66@yandex.ru>
2022-10-03 01:03:50 +0300

    Lession_5_6

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,9 +2,16 @@ import '../App.css';
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import {TextField, Button} from "@material-ui/core";
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Favorite from '@material-ui/icons/Favorite';
+import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import { showNameAction } from './store/profile/actions';
+import { useSelector, useDispatch } from 'react-redux';
+
 
 
 const useStyles = makeStyles({
@@ -17,12 +24,25 @@ const useStyles = makeStyles({
   text: {
     fontSize: 12,
     textAlign: 'center'
+  },
+  form: {
+    display: 'flex',
+    flexDirection: 'column'
   }
-  
 });
 
 function Profile (){
     const classes = useStyles(); 
+    const iSshowName = useSelector((state) => state.showName);
+    const dispatch = useDispatch()
+    
+    
+    const setShowName = () => {
+      dispatch(showNameAction)
+
+    };
+  
+
   
     return (
  
@@ -32,9 +52,17 @@ function Profile (){
   You Profile
   </Typography>
      
-    <form >
-        <TextField id="outlined-basic" label="Fist Name" />
-        <TextField id="outlined-basic" label="Second Name" />
+    <form className={classes.form}>
+        <TextField id="outlined-basic" label= 'Name'/>
+        {iSshowName && 
+        <TextField id="outlined-basic" label="Second Name" name="checkedH"/>}
+        <FormControlLabel
+        control={<Checkbox icon={<FavoriteBorder />} checkedIcon={<Favorite />} name="checkedH" 
+        onChange={setShowName}
+        />}
+        label="Show Second Name?"
+      />  
+        
         <TextField id="outlined-basic" label="Age" />
           <Button style={{margin: '20px'}} variant="contained" type="button" color="secondary">
             Save
